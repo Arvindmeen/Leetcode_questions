325e017d066492dfa72a08456f78e8fd9e3a67e0
Arvind Meena <162046927+Arvindmeen@users.noreply.github.com>
2026-08-05 00:17:10 +0530

    Time: 12 ms (6.06%), Space: 33.9 MB (8.08%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -10,13 +10,11 @@ public:
         for(int i = 0; i<n; i++){
             mpp[nums[i]];
         }
-
         for(int i = start; i<=end; i++){
             if(mpp.find(i) == mpp.end()){
                 ans.push_back(i);
             }
         }
-
         return ans;
     }
 };
