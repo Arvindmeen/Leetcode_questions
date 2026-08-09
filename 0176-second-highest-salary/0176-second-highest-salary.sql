fbdb5d0a73299526d12dab27b92408d1c70550e4
Arvind Meena <162046927+Arvindmeen@users.noreply.github.com>
2026-08-09 20:21:56 +0530

    Time: 1160 ms (5%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,15 @@
 # Write your MySQL query statement below
-SELECT(
-    SELECT DISTINCT salary FROM Employee
+-- SELECT(
+--     SELECT DISTINCT salary FROM Employee
+--     ORDER BY salary DESC
+--     LIMIT 1 OFFSET 1
+-- ) AS SecondHighestSalary;
+
+
+-- by colaesc
+SELECT COALESCE(
+   (SELECT DISTINCT salary FROM Employee
     ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
+    LIMIT 1 OFFSET 1),
+    NULL
 ) AS SecondHighestSalary;
